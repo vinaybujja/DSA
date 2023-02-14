3328cf6fb8e1059980be86e148096616c15a667a
vinaybujja <122912206+vinaybujja@users.noreply.github.com>
2023-02-14 23:53:29 +0530

    Create travesre_linked_list.cxx

---
+++ c++/linked list/travesre_linked_list.cxx
@@ -0,0 +1,42 @@
+//creating a Linked list
+//this program creates linked list with two nodes in it.
+//result - prints value 1 2 3 on screen
+
+#include<iostream>
+using namespace std;
+
+//creating node
+struct node
+{
+    int val;
+    node *nxt;
+};
+
+//creating function to traverse list
+//time complexcity = O(n)
+//space complexcity = O(1)
+void traverse(node *head)
+{
+ while (head!=NULL)
+ {
+    cout<<head->val<<endl;
+    head = head->nxt;
+ }
+}
+
+// Main function
+int main()
+{
+    node *head, *first, *second;    //creating pointer of data type node
+    head = new node();              //dynamic memory allocation
+    first = new node();             //dynamic memory allocation
+    second = new node();            //dynamic memory allocation
+    head->val = 1;                  //giving value to head node
+    head->nxt = first;              //connecting head node to first node
+    first->val = 2;                 //giving value to first node
+    first->nxt = second;            //connecting first node to second node
+    second->val = 3;                //giving value to second node
+    second->nxt = NULL;             //allocating NULL value to last(second) node
+    traverse(head);                 //calling traverse function
+    return 0;
+}
